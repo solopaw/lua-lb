c1caff4419a3049e65e010a3118edde2466b131e
solopaw <41173209+solopaw@users.noreply.github.com>
2019-02-03 07:43:06 +1000

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -1,1 +1,15 @@
-game.Players.PlayerAdded:Connect(function(Player)
+
+local function onPlayerJoin(player)
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+ 
+	-- Display an 'IntValue' on leaderboard
+	local gold = Instance.new("IntValue")
+	gold.Name = "Gold"
+	gold.Value = 0
+	gold.Parent = leaderstats
+end
+ 
+-- Run 'onPlayerJoin()' when the 'PlayerAdded' event fires
+game.Players.PlayerAdded:Connect(onPlayerJoin)
